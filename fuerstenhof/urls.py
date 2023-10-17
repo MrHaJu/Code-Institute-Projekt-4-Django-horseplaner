c361c471c8a218bab92ec98eec4be6ed3080f04e
MrHaJu <134605064+MrHaJu@users.noreply.github.com>
2023-10-17 16:48:11 +0200

    add allauth url path and App

--- fuerstenhof/urls.py
+++ fuerstenhof/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('horse_planer.urls'), name='horse_planer_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
