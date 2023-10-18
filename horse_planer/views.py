fe4d99cf8960586ed1290634925ec43a7b280704
MrHaJu <134605064+MrHaJu@users.noreply.github.com>
2023-10-18 23:01:49 +0200

    add post like in views

--- horse_planer/views.py
+++ horse_planer/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.views import generic, View
-from .models import Post
-from .forms import CommentForm
+from django.http import HttpResponseRedirect
+from .models import Post, UserPost
+from .forms import CommentForm, UserPostForm
+
+
 
 
 class PostList(generic.ListView):
@@ -63,3 +66,16 @@ class PostDetail(View):
                 "comment_form": CommentForm()
             },
         )
+        
+class PostLike(View):
+    
+    def post(self, request, slug):
+        post = get_object_or_404(Post, slug=slug)
+        
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
+        
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+    
