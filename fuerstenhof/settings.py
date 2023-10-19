aeabc7d4e353d7219f85d43b61f1ad8e2498b352
MrHaJu <134605064+MrHaJu@users.noreply.github.com>
2023-10-19 17:16:10 +0200

    update settings

--- fuerstenhof/settings.py
+++ fuerstenhof/settings.py
@@ -57,14 +57,10 @@ INSTALLED_APPS = [
 SITE_ID = 1
 
 STATIC_URL = '/static/'
+MEDIA_URL = 'media/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 ACCOUNT_EMAIL_VERIFICATION = 'none'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 MESSAGE_TAGS = {
@@ -162,7 +158,8 @@ STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
