a37f7cd01cd650a676eb1724b54b0807da1eedd1
MrHaJu <134605064+MrHaJu@users.noreply.github.com>
2023-10-20 11:00:58 +0200

    update settings

--- fuerstenhof/settings.py
+++ fuerstenhof/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uaare=dk1f@f+z(y5n2o+dx6bn^u4#)a=$1!%u##)h72_)kcmh'
+SECRET_KEY = os.environ.get("SECRET_KEY", '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
@@ -62,7 +62,7 @@ INSTALLED_APPS = [
 SITE_ID = 1
 
 STATIC_URL = '/static/'
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 ACCOUNT_EMAIL_VERIFICATION = 'none'
