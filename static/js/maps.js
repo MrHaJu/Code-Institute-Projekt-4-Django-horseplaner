d28aad0afb17dc5f2be963c9a87917565d345399
MrHaJu <134605064+MrHaJu@users.noreply.github.com>
2023-10-20 19:11:07 +0200

    add google map

--- static/js/maps.js
+++ static/js/maps.js
@@ -2,12 +2,12 @@ let map;
 
 async function initMap() {
   //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps");
+    const { Map } = await google.maps.importLibrary("maps");
 
-  map = new Map(document.getElementById("map"), {
+    map = new Map(document.getElementById("map"), {
     center: { lat: -34.397, lng: 150.644 },
     zoom: 8,
-  });
+    });
 }
 
 initMap();
