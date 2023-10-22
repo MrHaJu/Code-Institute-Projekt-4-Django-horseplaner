a617350616f2e19c49c74950ee37060839766846
MrHaJu <134605064+MrHaJu@users.noreply.github.com>
2023-10-22 19:48:45 +0200

    add path for edit and delete comment

--- horse_planer/urls.py
+++ horse_planer/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('post-detail/<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
     path('horses/', views.HorsesView.as_view(), name='horses'),
+    path('comments/<int:comment_id>/edit/', views.edit_comment, name='edit_comment'),
+    path('comments/<int:comment_id>/delete/', views.delete_comment, name='delete_comment'),
 ]
