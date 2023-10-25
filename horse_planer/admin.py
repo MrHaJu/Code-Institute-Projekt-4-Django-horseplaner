84e8c50e296adfdf1403530a7ef20f093aaf0762
MrHaJu <134605064+MrHaJu@users.noreply.github.com>
2023-10-25 11:50:18 +0200

    fix bug in delete comment.

--- horse_planer/admin.py
+++ horse_planer/admin.py
@@ -15,8 +15,7 @@ class PostAdmin(SummernoteModelAdmin):
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'body', 'post', 'created_on', 'approved')
-    list_display = ('name', 'post', 'created_on', 'approved')
+    list_display = ('name', 'email', 'body', 'post', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
     search_fields = ('name', 'email', 'body')
     actions = ['approve_comments']
--- horse_planer/models.py
+++ horse_planer/models.py
@@ -33,12 +33,11 @@ class Post(models.Model):
     def number_of_likes(self):
         return self.likes.count()
 
-
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=80)
-    email = models.EmailField()
+    email = models.EmailField(blank=False)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
@@ -48,3 +47,4 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment {self.body} by {self.name}"
+
