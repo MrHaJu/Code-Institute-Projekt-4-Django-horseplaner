cd245e3a68375a5b323bffe1d35384f289a2a0f8
MrHaJu <134605064+MrHaJu@users.noreply.github.com>
2023-10-27 10:11:31 +0200

    create eventlistener in js for go back button

---
+++ static/js/goback.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const goBackButton = document.getElementById("goBackButton");
+    if (goBackButton) {
+        goBackButton.addEventListener("click", function () {
+            window.history.back();
+        });
+    }
+});
